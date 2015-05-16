7733b28b37e514a676237c5595e9f9639f6306b1
bluepichu <bluepichu64@gmail.com>
2015-05-16 16:55:51 -0400

    Twilio and AsyncHandler

--- server/serv.js
+++ server/serv.js
@@ -6,7 +6,7 @@ var http = require("http").Server(app);
 var path = require("path");
 var db = require("./db");
 var ObjectId = db.ObjectId;
-var extend = require("extend");
+var twilio = require("./twilio");
 
 app.get("/css/:file", function(req, res){
 	res.sendFile("/css/" + req.params.file, {root: path.join(__dirname, "../public")});
@@ -14,6 +14,17 @@ app.get("/css/:file", function(req, res){
 
 app.get("/js/:file", function(req, res){
 	res.sendFile("/js/" + req.params.file, {root: path.join(__dirname, "../public")});
+});
+
+app.get("/twilio", function(req, res){ // testing only
+	twilio.send(["+17032096667", "+17032096667"], "Oh well", function(err, data){
+		if(!err){
+			console.log("success");
+		} else {
+			console.log("failure", err);
+		}
+	});
+	res.send("Message sent.");
 });
 
 http.listen(process.env.PORT || 1337, function(){});
