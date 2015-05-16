cbc650df27d708fcbd4b1545521aea747155176c
addtheletters <bzhang.2003@gmail.com>
2015-05-16 19:25:00 -0400

    doing references correct

--- server/pingdb.js
+++ server/pingdb.js
@@ -14,36 +14,36 @@ db.once('open', function (callback) {
 	var channelSchema = mongoose.Schema({
 		name		: String,
 		subscribed	: [mongoose.Schema.Types.Mixed], // all subscribed to the channel; users and loose phones
-		event		: Event,
-		subChannels	: [Channel] // notification to a channel is also sent to all subchannels
+		event		: { type: mongoose.Schema.ObjectId, ref: 'Event' },
+		subChannels	: [{ type: mongoose.Schema.ObjectId, ref: 'Channel' }] // notification to a channel is also sent to all subchannels
 	});
 	var userSchema = mongoose.Schema({
 		name		: String,
 		phone		: String,
-		channels	: [Channel], // channels
-		organized	: [Event], // events the user is running
-		participated: [Event], // events the user is going to
-		spectated	: [Event] // events the user is spectating
+		channels	: [{ type: mongoose.Schema.ObjectId, ref: 'Channel' }], // channels
+		organized	: [{ type: mongoose.Schema.ObjectId, ref: 'Event' }], // events the user is running
+		participated: [{ type: mongoose.Schema.ObjectId, ref: 'Event' }], // events the user is going to
+		spectated	: [{ type: mongoose.Schema.ObjectId, ref: 'Event' }] // events the user is spectating
 	});
 	var eventSchema = mongoose.Schema({
 		name		: String,
 		description	: String,
 		phones		: [String], // for loose phones that have subscribed to something in the event
-		channels	: [Channel],
-		participants: [User],
-		spectators	: [User], // users who subscribe to something in the event?
-		subEvents	: [SubEvent]
+		channels	: [{ type: mongoose.Schema.ObjectId, ref: 'Channel' }],
+		participants: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
+		spectators	: [{ type: mongoose.Schema.ObjectId, ref: 'User' }], // users who subscribe to something in the event?
+		subEvents	: [{ type: mongoose.Schema.ObjectId, ref: 'SubEvent' }]
 		// time		: Date
 	});
 	var subEventSchema = mongoose.Schema({ // match, heat, or panel
 		name		: String,
 		description	: String,
 		
-		parentEvent : Event,
+		parentEvent : { type: mongoose.Schema.ObjectId, ref: 'Event' },
 		
-		channels	: [Channel],
-		participants: [User],
-		spectators	: [User],
+		channels	: [{ type: mongoose.Schema.ObjectId, ref: 'Channel' }],
+		participants: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
+		spectators	: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
 		time		: Date,
 		// endTime	: Date
 	});
